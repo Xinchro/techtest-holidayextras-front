82506ccef8d526067e1391b9937b707fb4f24592
Xinchro <TheXrocks@gmail.com>
2018-03-12 18:57:47 +0000

    added vue

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+let app = new Vue({
+  el: '#vue-app',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    start: function () {
+      console.log("Vue running!")
+    }
+  }
+})
+
+// launch app
+app.start()
