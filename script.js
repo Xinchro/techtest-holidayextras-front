7da8f7e1305aad2ca075037253beb2fd00f2c299
Xinchro <TheXrocks@gmail.com>
2018-03-13 11:28:03 +0000

    added importing of flickr json

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 let app = new Vue({
-  el: '#vue-app',
+  el: "#vue-app",
   data: {
-    message: 'Hello Vue.js!'
+    flickrUrl: "https://api.flickr.com/services/feeds/photos_public.gne?format=json",
+    flickrJSON: {}
   },
   methods: {
-    start: function () {
-      console.log("Vue running!")
+    start() {
+      this.getFlickrJSON()
+        .then((response) => {
+          this.flickrJSON = response
+          console.log(response)
+        })
+    },
+    getFlickrJSON() {
+      return fetchJSONP(this.flickrUrl, "jsonFlickrFeed")
+        .then(function(response) {
+            return response.json()
+          })
+        .catch(function(ex) {
+            console.log('parsing failed', ex)
+          })
     }
   }
 })
