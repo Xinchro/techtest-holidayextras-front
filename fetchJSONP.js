6367c644e9cc1170ef144772e4bea0a8d8460085
Xinchro <TheXrocks@gmail.com>
2018-03-13 11:23:26 +0000

    simple JSONP fetching function

---
+++ fetchJSONP.js
@@ -0,0 +1,17 @@
+function fetchJSONP(url, callbackName) {
+ return new Promise((res, rej) => {
+   let script = document.createElement('script')
+
+   if (url.match(/\?/)) url += "&callback="+callbackName
+   else url += "?callback="+callbackName
+
+   script.src = url
+   window[callbackName] = (json) => {
+     res(new Response(JSON.stringify(json)))
+     script.remove()
+     delete window[callbackName]
+   }
+
+   document.body.appendChild(script)
+ })
+}
