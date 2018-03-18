73a40ff00b722649740f0b7b9291580fdac9f09a
Xinchro <TheXrocks@gmail.com>
2018-03-18 17:37:04 +0000

    added much needed comments (shame on me) and did some general tweaking and cleaning up

--- components/tags/script.js
+++ components/tags/script.js
@@ -15,33 +15,43 @@ export default Vue.component("tagsPage", {
   },
   beforeCreate: function () {
     this.$nextTick(function () {
+      // an initial search
       this.start()
     })
   },
   methods: {
+    /*
+      Initiates a default search when the page loads
+    */
     start() {
+      // initial (default) search
       this.search = "cat"
       this.getPhotosByTags(this.search)
     },
 
+    /*
+      Turns a string of space-divided tags into an array
+      @params string - space separated tags
+      @returns array - empty or array of tags
+    */
     formatTags(tagString) {
       if(tagString) return tagString.split(" ")
 
       return []
     },
 
-    formatAuthor(authorString) {
-      if(authorString) return authorString.split("\"")[1]
-
-      return "Author"
-    },
-
+    /*
+      Formats a string as "DD MMMM YYYY, hh:mm:ss"
+      @params number - epoch time
+      @returns string - formatted datetime
+    */
     formatDate(isoDate) {
       const date = new Date(isoDate)
       const MONTH_NAMES = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
       ]
 
+      // do the formatting properly
       let dateString = ""
 
       dateString = dateString.concat(date.getFullYear())
@@ -64,30 +74,55 @@ export default Vue.component("tagsPage", {
       return dateString
     },
 
-    getDescription(descriptionString) {
-      let template = document.createElement("div")
+    /*
+      Gets the description string from the description object
 
-      template.innerHTML = descriptionString
+      @params object - the object with the description text in it
+      @returns string - a description
+    */
+    getDescription(description) {
+      // create a temporary element
+      let section = document.createElement("section")
+      // set the html in our temp element
+      section.innerHTML = description._content || "No content"
 
-      if(template.children[2]) {
-        return template.children[2].innerText
-      }
-
-      return "No description"
+      // return the text(only) from our temp element
+      return section.textContent || section.innerText || ""
     },
 
+    /*
+      Pads single digit numbers with a zero
+
+      @params number
+      @returns number
+    */
     padTens(number) {
       return number >= 10 ? number : `0${number}`
     },
 
+    /*
+      Makes sure there's a title
+
+      @params string
+      @returns string
+    */
     titleExists(title) {
       if(title.length > 1) return title
+      // if no title, return a stock string
       return "No Title"
     },
 
+    /*
+      Fetch JSON data from the flickr API
+
+      @params string - api endpoint url
+      @params string - callback name
+      @returns Promise - resolves with data
+    */
     getFlickrJSON(url, callback) {
       return fetchJSONP(url, callback)
         .then(function(response) {
+          // return promise with json in it
           return response.json()
         })
         .catch(function(err) {
@@ -95,62 +130,96 @@ export default Vue.component("tagsPage", {
         })
     },
 
+    /*
+      Creates a url to a photo's page based on the photo's and owner's ids
+
+      @params string - photo id
+      @params string - owner id
+      @returns string - photo page url
+    */
     getPhotoURL(photoID, ownerID) {
       return `https://www.flickr.com/${ownerID}/${photoID}`
     },
 
+    /*
+      Get a page(up to 100) of photos from flickr, based on the tags, and add it to the photo collection
+
+      @params string - space separated tags
+    */
     getPhotosByTags(tags) {
+      // set the system to busy until we get the new photos
+      this.busy = true
       let photos = []
 
+      // turn our tags into an array
       tags = tags.replace(/\ /g, ",")
 
+      // set up our queries
       const method = "flickr.photos.search"
-
       const extras = "date_taken,description,owner_name,tags,url_m"
-
       const format = "format=json"
-
       const page = `page=${this.currentPage}`
 
+      // join all our queries together
       const queries = `method=${method}&api_key=${window.flickrTokens.Key}&tags=${tags}&extras=${extras}&${page}`
 
+      // setup the request url
       const request = `${this.flickrApiUrl}?${queries}&${format}`
 
+      // get our json with our new url and expected callback function name
       this.getFlickrJSON(request, "jsonFlickrApi")
         .then((response) => {
           let photoArray = response.photos.photo
+          // make a new property per photo, for its page url
           photoArray.map((photo) => {
             photo.link = this.getPhotoURL(photo.id, photo.owner)
           })
+
+          // add the new page to the current list
           this.flickrJSON.items = this.flickrJSON.items.concat(photoArray)
-          console.log(JSON.stringify(this.flickrJSON.items[0]))
+          // got our photos, not busy anymore
+          this.busy = false
         })
     },
 
+    /*
+      Adds 10 images to the displayed photo array, gets a new page once limit(100) is reached
+    */
     loadMore() {
-      this.busy = true;
-      let pageLimit = this.currentPage * 100 // flickr sends 100 per page
+      // if we're busy: don't add anything new
+      if(!this.busy) {
+        // set us as busy
+        this.busy = true
 
-      setTimeout(() => {
+        // check to see if our new set of 10 photos will make us go over the total photos we have
         if(this.lastImageIndex+10 < this.flickrJSON.items.length) {
+          // loop through and add 10 new photos to display
           for(let i=this.lastImageIndex; i<this.lastImageIndex+10; i++) {
             this.displayJSON.items.push(this.flickrJSON.items[i])
           }
+          // increment current index
           this.lastImageIndex += 10
-        } else
-        if(this.lastImageIndex === this.flickrJSON.items.length) {
+        }
+        // if we've reached the end, try get a new page
+        else if(this.lastImageIndex === this.flickrJSON.items.length) {
           // limit reached, next page please
           this.currentPage++
+          // get photos(in new page)
           this.getPhotosByTags(this.search)
-        } else {
+        }
+        // if we're going to go over our total
+        else {
+          // loop until our total and add the remaining pictures
           for(let i=this.lastImageIndex; i<this.flickrJSON.items.length; i++) {
             this.displayJSON.items.push(this.flickrJSON.items[i])
           }
+          // current index is set to our limit
           this.lastImageIndex = this.flickrJSON.items.length
         }
 
+        // loaded new images, not busy anymore
         this.busy = false
-      }, 1000)
+      }
     }
   }
 })
